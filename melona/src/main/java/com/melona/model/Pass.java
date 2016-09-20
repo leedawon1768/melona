230686c296421977cf4f7824874c70d98b99f166
gumin <lgm408@naver.com>
2016-09-20 11:39:05 +0900

    5번 파트 vo 제작 완료

---
+++ melona/src/main/java/com/melona/model/Pass.java
@@ -0,0 +1,43 @@
+package com.melona.model;
+
+public class Pass {
+
+	private int aassNo;
+	private int downloadCount;
+	private String name;
+	private int price;
+	private int shoppingNo;
+	
+	public int getAassNo() {
+		return aassNo;
+	}
+	public void setAassNo(int aassNo) {
+		this.aassNo = aassNo;
+	}
+	public int getDownloadCount() {
+		return downloadCount;
+	}
+	public void setDownloadCount(int downloadCount) {
+		this.downloadCount = downloadCount;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getShoppingNo() {
+		return shoppingNo;
+	}
+	public void setShoppingNo(int shoppingNo) {
+		this.shoppingNo = shoppingNo;
+	}
+	
+	
+}
