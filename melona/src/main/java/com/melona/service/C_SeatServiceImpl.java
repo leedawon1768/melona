611b39ea1d4f5c2567b9667df77b624af53baeed
'kimjieun62' <kimjieun6226@gmail.com>
2016-09-20 11:02:32 +0900

    service commit

---
+++ melona/src/main/java/com/melona/service/C_SeatServiceImpl.java
@@ -0,0 +1,14 @@
+package com.melona.service;
+
+import java.util.List;
+
+import com.melona.model.C_Seat;
+
+public class C_SeatServiceImpl implements C_SeatService {
+
+	@Override
+	public List<C_Seat> getSeat() {
+		return null;
+	}
+
+}
