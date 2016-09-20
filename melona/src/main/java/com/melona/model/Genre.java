d06ff75bf97087aeed9582714681548107ef5f62
Jung Bo <hyuna_87@naver.com>
2016-09-20 12:29:41 +0900

    다운로드내역/장르/멤버/멜론Q&A model추가

---
+++ melona/src/main/java/com/melona/model/Genre.java
@@ -0,0 +1,27 @@
+package com.melona.model;
+
+public class Genre {
+
+	private int genre_no;
+	private String name;
+	
+	public Genre() {}
+
+	public int getGenre_no() {
+		return genre_no;
+	}
+
+	public void setGenre_no(int genre_no) {
+		this.genre_no = genre_no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ melona/src/main/java/com/melona/model/Mqna.java
@@ -0,0 +1,74 @@
+package com.melona.model;
+
+import java.util.Date;
+
+public class Mqna {
+
+	private int mqna_no;
+	private String title;
+	private String memo;
+	private String type1;
+	private String type2;
+	private int member_no;
+	private Date regdate;
+	
+	public Mqna() {}
+
+	public int getMqna_no() {
+		return mqna_no;
+	}
+
+	public void setMqna_no(int mqna_no) {
+		this.mqna_no = mqna_no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public String getType1() {
+		return type1;
+	}
+
+	public void setType1(String type1) {
+		this.type1 = type1;
+	}
+
+	public String getType2() {
+		return type2;
+	}
+
+	public void setType2(String type2) {
+		this.type2 = type2;
+	}
+
+	public int getMember_no() {
+		return member_no;
+	}
+
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+}
