96b4460b21e5daffd2b3072fc3e980bda1e61a0d
kimkh525 <kimkh525@naver.com>
2016-09-20 12:34:45 +0900

    no 6

---
+++ melona/src/main/java/com/melona/model/PlayList.java
@@ -0,0 +1,22 @@
+package com.melona.model;
+
+public class PlayList {
+
+	private MyAlbum myalbumNo;
+	private Music musicNo;
+	
+	public MyAlbum getMyalbumNo() {
+		return myalbumNo;
+	}
+	public void setMyalbumNo(MyAlbum myalbumNo) {
+		this.myalbumNo = myalbumNo;
+	}
+	public Music getMusicNo() {
+		return musicNo;
+	}
+	public void setMusicNo(Music musicNo) {
+		this.musicNo = musicNo;
+	}
+	
+	
+}
---
+++ melona/src/main/java/com/melona/model/ShoppingList.java
@@ -0,0 +1,44 @@
+package com.melona.model;
+
+import java.util.Date;
+
+public class ShoppingList {
+
+	private int no;
+	private int price;
+	private Date start_date;
+	private Date end_date;
+	private Members memberNo;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	public Members getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(Members memberNo) {
+		this.memberNo = memberNo;
+	}
+	
+	
+}
