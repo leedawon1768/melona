5c24dd13b2d6745d934e3e73548349f432dd55a2
leedawon1768 <ldw2727@hanmail.net>
2016-09-20 16:05:10 +0900

    commit

--- melona/src/main/java/com/melona/service/MelonaService.java
+++ melona/src/main/java/com/melona/service/MelonaService.java
@@ -1,9 +1,5 @@
 package com.melona.service;
 
-import java.util.List;
-
-import com.melona.model.*;
-
 public interface MelonaService {
 	
 	
--- melona/src/main/java/com/melona/service/MelonaServiceImpl.java
+++ melona/src/main/java/com/melona/service/MelonaServiceImpl.java
@@ -4,11 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.melona.model.Album;
-import com.melona.model.C_Concert;
-import com.melona.model.Music;
-import com.melona.model.Singer;
-
 @Service
 public class MelonaServiceImpl implements MelonaService {
 
