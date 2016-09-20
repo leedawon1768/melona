e0c58ef2de444ffe892d27841bc2d72566d0ab39
'sunghee' <sunghee5003@gmail.com>
2016-09-20 12:04:58 +0900

    commit~~~

--- melona/src/main/java/com/melona/service/AdminServiceImpl.java
+++ melona/src/main/java/com/melona/service/AdminServiceImpl.java
@@ -1,5 +1,20 @@
 package com.melona.service;
 
-public class AdminServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+	
+	// 가수 등록
+	
+	// 앨범 등록
+	
+	// 음악 등록
+	
+	// 공연 등록
+	
+	// 공연장 등록
+	
+	// 이용권 등록
 
 }
